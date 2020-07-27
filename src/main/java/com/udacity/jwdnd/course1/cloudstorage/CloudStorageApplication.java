6b879ba2195c305c5bfb2fce4cabdcb39415d2f1
Agoston PAJZS <agostonp@gmail.com>
2020-07-27 15:54:03 +0200

    Add a default user to skip signup for every test

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -1,7 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudStorageApplication {
@@ -10,4 +14,11 @@ public class CloudStorageApplication {
 		SpringApplication.run(CloudStorageApplication.class, args);
 	}
 
+	@Bean
+	public static User defaultUser(UserService userService) {
+		System.out.println("defaultUser Bean created");
+		User user = new User(null, "d", null, "1", "Default", "Bacsi");
+		userService.createUser(user);
+		return user;
+	}
 }
